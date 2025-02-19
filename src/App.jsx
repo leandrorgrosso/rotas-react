f0e01ff9c4d1fd70ff0bbde8209c1164f0e99668
leandrorgrosso <leandrorgrosso@gmail.com>
2025-02-19 10:47:23 -0300

    link do router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 
 function App() {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">🏠 Home</Link></li>
+          <li><Link to="/sobre">ℹ️ Sobre</Link></li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
